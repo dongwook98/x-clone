9aeef7bed7e027937af9587c475b694032d93b30
강동욱 <ehddnr8563@gmail.com>
2024-02-05 16:42:23 +0900

    explore 페이지 UI 구현
    - 레이아웃에 있던 검색창과 트렌드 메인 컨텐츠로 옮김

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,3 +1,26 @@
+import styles from './page.module.css';
+import SearchForm from '../_components/SearchForm';
+import Trend from '../_components/Trend';
+
 export default function ExplorePage() {
-  return <>탐색 페이지</>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.formZone}>
+        <SearchForm />
+      </div>
+      <div className={styles.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
