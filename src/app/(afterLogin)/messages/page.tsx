df669e0b1391adba0dd8fbe437757520fd2da73e
강동욱 <ehddnr8563@gmail.com>
2024-02-06 20:53:23 +0900

    message 페이지와 상세 메시지 페이지 구현

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <>메시지 페이지</>;
+import style from './page.module.css';
+import Room from '@/app/(afterLogin)/messages/_components/Room';
+
+export default function MessagesPage() {
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+    </main>
+  );
 }
