3c6b57a7b137664550574b551ac5962b7e24da3b
강동욱 <ehddnr8563@gmail.com>
2024-02-06 20:17:14 +0900

    search 페이지 구현

---
+++ src/app/(afterLogin)/search/_components/Tab.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import styles from './Tab.module.css';
+import { useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+export default function Tab() {
+  const [current, setCurrent] = useState('hot');
+  const router = useRouter();
+  const searchParams = useSearchParams(); // searchParams를 가져올 수 있음
+
+  const onClickHot = () => {
+    setCurrent('hot');
+    router.replace(`/search?q=${searchParams.get('q')}`);
+  };
+
+  const onClickNew = () => {
+    setCurrent('new');
+    router.replace(`/search?${searchParams.toString()}&f=live`);
+  };
+
+  return (
+    <div className={styles.homeFixed}>
+      <div className={styles.homeTab}>
+        <div onClick={onClickHot}>
+          인기
+          <div className={styles.tabIndicator} hidden={current === 'new'}></div>
+        </div>
+        <div onClick={onClickNew}>
+          최신
+          <div className={styles.tabIndicator} hidden={current === 'hot'}></div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,3 +1,42 @@
-export default function SearchPage() {
-  return <>검색 페이지</>;
+import styles from './page.module.css';
+import BackButton from '@/app/(afterLogin)/_components/BackButton';
+import SearchForm from '@/app/(afterLogin)/_components/SearchForm';
+import Tab from '@/app/(afterLogin)/search/_components/Tab';
+import Post from '@/app/(afterLogin)/_components/Post';
+
+type Props = {
+  searchParams: { q: string; f?: string; pf?: string };
+};
+
+// searchParams가 기본적으로 들어있음
+export default function SearchPage({ searchParams }: Props) {
+  return (
+    <main className={styles.main}>
+      <div className={styles.searchTop}>
+        <div className={styles.searchZone}>
+          <div className={styles.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={styles.formZone}>
+            <SearchForm q={searchParams.q} />
+          </div>
+        </div>
+        <Tab />
+      </div>
+      <div className={styles.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        {/*<SearchResult searchParams={searchParams} />*/}
+      </div>
+    </main>
+  );
 }
