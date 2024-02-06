8882e4eb7ceec7bf2d9b1fc09e73f7037a9d72c2
강동욱 <ehddnr8563@gmail.com>
2024-02-06 20:23:00 +0900

    상세 게시글 페이지 구현

--- src/app/(afterLogin)/[username]/status/[postId]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[postId]/page.tsx
@@ -1,3 +1,29 @@
+import BackButton from '@/app/(afterLogin)/_components/BackButton';
+import styles from './page.module.css';
+import Post from '@/app/(afterLogin)/_components/Post';
+import CommentForm from './_components/CommentForm';
+
 export default function DetailPostPage() {
-  return <>상세 게시글 페이지</>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <BackButton />
+        <h3 className={styles.headerTitle}>게시하기</h3>
+      </div>
+      <Post />
+      <CommentForm />
+      <div>
+        {/* 댓글들 */}
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
 }
