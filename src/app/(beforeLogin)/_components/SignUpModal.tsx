64632a44b91adf32a3022069c99102b69f66d7a1
강동욱 <ehddnr8563@gmail.com>
2024-03-27 22:34:01 +0900

    Feat: 회원가입 서버컴포넌트 - 서버액션으로 구현

--- src/app/(beforeLogin)/_components/SignUpModal.tsx
+++ src/app/(beforeLogin)/_components/SignUpModal.tsx
@@ -1,140 +1,121 @@
-'use client';
-
-import style from './SignUpModal.module.css';
-import { useRouter } from 'next/navigation';
-import { ChangeEventHandler, FormEventHandler, useState } from 'react';
+import { redirect } from 'next/navigation';
+import BackButton from './BackButton';
+import styles from './SignUpModal.module.css';
 
 export default function SignUpModal() {
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
-  const [nickname, setNickname] = useState('');
-  const [image, setImage] = useState('');
-  const [imageFile, setImageFile] = useState<File>();
-
-  const router = useRouter();
-  const onClickClose = () => {
-    router.back();
-    // TODO: 뒤로가기가 /home이 아니면 /home으로 보내기
-  };
-
-  const onChangeId: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setId(e.target.value);
-  };
-
-  const onChangePassword: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setPassword(e.target.value);
-  };
-  const onChangeNickname: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setNickname(e.target.value);
-  };
-  const onChangeImageFile: ChangeEventHandler<HTMLInputElement> = (e) => {
-    e.target.files && setImageFile(e.target.files[0]);
-  };
-
-  const onSubmit: FormEventHandler = (e) => {
-    e.preventDefault();
-    fetch('http://localhost:9090/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        id,
-        nickname,
-        image,
-        password,
-      }),
-      credentials: 'include',
-    })
-      .then((response: Response) => {
-        console.log(response.status);
-        if (response.status === 200) {
-          router.replace('/home');
+  // 서버 컴포넌트에서 서버 액션 사용 방법
+  // 프론트 서버에서 실행되기 때문에 비밀키 값과 같은 코드 작성 가능
+  // formData 사용하려면 input name 속성 필요
+  const submit = async (formData: FormData) => {
+    'use server'; // 서버 액션 사용하려면 작성
+    if (!formData.get('id')) {
+      return { message: 'no_id' };
+    }
+    if (!formData.get('name')) {
+      return { message: 'no_name' };
+    }
+    if (!formData.get('password')) {
+      return { message: 'no_password' };
+    }
+    if (!formData.get('image')) {
+      return { message: 'no_image' };
+    }
+    let shouldRedirect = false; // try catch문에서 redirect 사용 못해서 변수 생성
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/users`,
+        {
+          method: 'post',
+          body: formData,
+          credentials: 'include', // 이게 있어야 쿠키 전달 가능
         }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+      );
+      console.log(response.status);
+      // 백엔드 서버에서 문제 발생 시 처리
+      if (response.status === 403) {
+        return { message: 'user_exists' };
+      }
+      console.log(await response.json());
+      shouldRedirect = true;
+    } catch (error) {
+      console.error(error);
+      return;
+    }
+
+    if (shouldRedirect) {
+      redirect('/home'); // try catch문 안에서 X
+    }
   };
 
   return (
-    <>
-      <div className={style.modalBackground}>
-        <div className={style.modal}>
-          <div className={style.modalHeader}>
-            <button className={style.closeButton} onClick={onClickClose}>
-              <svg
-                width={24}
-                viewBox='0 0 24 24'
-                aria-hidden='true'
-                className='r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03'
-              >
-                <g>
-                  <path d='M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z'></path>
-                </g>
-              </svg>
-            </button>
-            <div>계정을 생성하세요.</div>
-          </div>
-          <form>
-            <div className={style.modalBody}>
-              <div className={style.inputDiv}>
-                <label className={style.inputLabel} htmlFor='id'>
-                  아이디
-                </label>
-                <input
-                  id='id'
-                  className={style.input}
-                  type='text'
-                  placeholder=''
-                  value={id}
-                  onChange={onChangeId}
-                />
-              </div>
-              <div className={style.inputDiv}>
-                <label className={style.inputLabel} htmlFor='name'>
-                  닉네임
-                </label>
-                <input
-                  id='name'
-                  className={style.input}
-                  type='text'
-                  placeholder=''
-                  value={nickname}
-                  onChange={onChangeNickname}
-                />
-              </div>
-              <div className={style.inputDiv}>
-                <label className={style.inputLabel} htmlFor='password'>
-                  비밀번호
-                </label>
-                <input
-                  id='password'
-                  className={style.input}
-                  type='password'
-                  placeholder=''
-                  value={password}
-                  onChange={onChangePassword}
-                />
-              </div>
-              <div className={style.inputDiv}>
-                <label className={style.inputLabel} htmlFor='image'>
-                  프로필
-                </label>
-                <input
-                  id='image'
-                  className={style.input}
-                  type='file'
-                  accept='image/*'
-                  onChange={onChangeImageFile}
-                />
-              </div>
-            </div>
-            <div className={style.modalFooter}>
-              <button className={style.actionButton} disabled>
-                가입하기
-              </button>
-            </div>
-          </form>
+    <div className={styles.modalBackground}>
+      <div className={styles.modal}>
+        <div className={styles.modalHeader}>
+          <BackButton />
+          <div>계정을 생성하세요.</div>
         </div>
+        <form action={submit}>
+          <div className={styles.modalBody}>
+            <div className={styles.inputDiv}>
+              <label className={styles.inputLabel} htmlFor='id'>
+                아이디
+              </label>
+              <input
+                id='id'
+                name='id'
+                className={styles.input}
+                type='text'
+                placeholder=''
+                required
+              />
+            </div>
+            <div className={styles.inputDiv}>
+              <label className={styles.inputLabel} htmlFor='name'>
+                닉네임
+              </label>
+              <input
+                id='name'
+                name='name'
+                className={styles.input}
+                type='text'
+                placeholder=''
+                required
+              />
+            </div>
+            <div className={styles.inputDiv}>
+              <label className={styles.inputLabel} htmlFor='password'>
+                비밀번호
+              </label>
+              <input
+                id='password'
+                name='password'
+                className={styles.input}
+                type='password'
+                placeholder=''
+                required
+              />
+            </div>
+            <div className={styles.inputDiv}>
+              <label className={styles.inputLabel} htmlFor='image'>
+                프로필
+              </label>
+              <input
+                id='image'
+                name='image'
+                className={styles.input}
+                type='file'
+                accept='image/*'
+                required
+              />
+            </div>
+          </div>
+          <div className={styles.modalFooter}>
+            <button type='submit' className={styles.actionButton}>
+              가입하기
+            </button>
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
